cc00f7e85f6aeea7a7f866ca6fc08a15fbfc57f0
Sergey Tarasov <deld67@gmail.com>
2020-03-03 18:38:37 +0300

    start java2 homework 4

---
+++ src/main/java/lesson4/myChat.java
@@ -0,0 +1,11 @@
+package lesson4;
+
+import javax.swing.*;
+
+public class myChat {
+    private JTextArea chatArea;
+    private JTextField inputText;
+    private JButton send;
+    private JList usersList;
+
+}
